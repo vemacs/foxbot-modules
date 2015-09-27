bb8c39465c8c4f74d658951b79c7d2212323a6c6
Lee Watson <rev@revthefox.co.uk>
2015-09-27 12:30:19 +0100

    Meme master tbh

---
+++ memes.py
@@ -0,0 +1,8 @@
+# coding=utf8
+from sopel.module import rule
+from sopel.formatting import bold
+
+@rule('^\[.+\]$')
+def intensify(bot, trigger):
+    bot.say(bold('[{0} INTENSIFIES]'.format(trigger.group(2).upper())))
+
