c297a833d5e686712ef3d5c06bd2ad7009c77cdc
Lee Watson <rev@revthefox.co.uk>
2015-09-27 13:36:04 +0100

    Add safebooru.py

--- safebooru.py
+++ safebooru.py
@@ -1,8 +1,8 @@
 # coding=utf8
 """
-gelbooru.py - People on IRC will love your leggings fetish!
+safebooru.py - People on IRC will love your leggings fetish!
 Copyright 2014 Max Gurela
-Adapted for use with sopel from https://github.com/infinitylabs/uguubot/blob/master/plugins/gelbooru.py
+Adapted for use with sopel from https://github.com/infinitylabs/uguubot/blob/master/plugins/safebooru.py
 
 Licensed under the Eiffel Forum License 2 (It's GPL compatible!).
 """
@@ -14,20 +14,20 @@ from bs4 import BeautifulSoup
 import random
 import re
 
-gelbooru_cache = []
+safebooru_cache = []
 lastsearch = ''
 
 
 def setup(bot):
-    regex = re.compile('gelbooru.com.*(?:\?|&)id\=([-_a-zA-Z0-9]+)')
+    regex = re.compile('safebooru.com.*(?:\?|&)id\=([-_a-zA-Z0-9]+)')
     if not bot.memory.contains('url_callbacks'):
         bot.memory['url_callbacks'] = tools.SopelMemory()
-    bot.memory['url_callbacks'][regex] = gelbooru_url
+    bot.memory['url_callbacks'][regex] = safebooru_url
 
 
 def refresh_cache(bot, inp):
-    global gelbooru_cache
-    gelbooru_cache = []
+    global safebooru_cache
+    safebooru_cache = []
     num = 0
     search = ''
     if inp == '':
@@ -36,38 +36,38 @@ def refresh_cache(bot, inp):
         search = inp.replace('explicit', 'rating:explicit').replace('nsfw', 'rating:explicit').replace('safe', 'rating:safe').replace('sfw', 'rating:safe')
     if not 'rating:' in search:
         search += ' rating:safe'
-    soup = get_soup('http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=10&tags={0}'.format(search))
+    soup = get_soup('http://safebooru.com/index.php?page=dapi&s=post&q=index&limit=10&tags={0}'.format(search))
     posts = soup.find_all('post')
 
     while num < len(posts):
-        gelbooru_cache.append((posts[num].get('id'), posts[num].get('score'), posts[num].get('file_url'), posts[num].get('rating'), posts[num].get('tags')))
+        safebooru_cache.append((posts[num].get('id'), posts[num].get('score'), posts[num].get('file_url'), posts[num].get('rating'), posts[num].get('tags')))
         num += 1
 
-    random.shuffle(gelbooru_cache)
+    random.shuffle(safebooru_cache)
     return
 
 
-@commands('gb', 'gelbooru')
-def gelbooru(bot, trigger):
+@commands('gb', 'safebooru')
+def safebooru(bot, trigger):
     """
-    .gelbooru <tags> -- Gets a random image, based on given tags from gelbooru.com
+    .safebooru <tags> -- Gets a random image, based on given tags from safebooru.com
     """
     global lastsearch
-    global gelbooru_cache
+    global safebooru_cache
 
     if trigger.group(2):
         search = trigger.group(2).strip().lower()
     else:
         search = ''
-    if not search in lastsearch or len(gelbooru_cache) < 2:
+    if not search in lastsearch or len(safebooru_cache) < 2:
         refresh_cache(bot, search)
     lastsearch = search
 
-    if len(gelbooru_cache) == 0:
+    if len(safebooru_cache) == 0:
         bot.say('No results for search "{0}"'.format(trigger.group(2).strip()))
         return
 
-    id, score, url, rating, tags = gelbooru_cache.pop()
+    id, score, url, rating, tags = safebooru_cache.pop()
 
     if 'e' in rating:
         rating = color('NSFW', colors.RED)
@@ -76,12 +76,12 @@ def gelbooru(bot, trigger):
     elif 's' in rating:
         rating = color('Safe', colors.GREEN)
 
-    bot.say('[gelbooru] Score: {0} | Rating: {1} | http://gelbooru.com/index.php?page=post&s=view&id={2} | Tags: {3}'.format(score, rating, id, tags.strip()))
+    bot.say('[safebooru] Score: {0} | Rating: {1} | http://safebooru.com/index.php?page=post&s=view&id={2} | Tags: {3}'.format(score, rating, id, tags.strip()))
 
 
-@rule(r'(?:.*)(?:gelbooru.com.*?id=)([-_a-zA-Z0-9]+)(?: .+)?')
-def gelbooru_url(bot, trigger):
-    soup = get_soup('http://gelbooru.com/index.php?page=dapi&s=post&q=index&id={0}'.format(trigger.group(1)))
+@rule(r'(?:.*)(?:safebooru.com.*?id=)([-_a-zA-Z0-9]+)(?: .+)?')
+def safebooru_url(bot, trigger):
+    soup = get_soup('http://safebooru.com/index.php?page=dapi&s=post&q=index&id={0}'.format(trigger.group(1)))
     posts = soup.find_all('post')
 
     id, score, url, rating, tags = (posts[0].get('id'), posts[0].get('score'), posts[0].get('file_url'), posts[0].get('rating'), posts[0].get('tags'))
@@ -93,7 +93,7 @@ def gelbooru_url(bot, trigger):
     elif 's' in rating:
         rating = color('Safe', colors.GREEN)
 
-    bot.say('[gelbooru] Score: {0} | Rating: {1} | Tags: {2}'.format(score, rating, tags.strip()))
+    bot.say('[safebooru] Score: {0} | Rating: {1} | Tags: {2}'.format(score, rating, tags.strip()))
 
 
 def get_soup(url):
