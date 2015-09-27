269db2c6e48bcc8e82343d73f3d35a335bbde96b
Lee Watson <rev@revthefox.co.uk>
2015-09-27 12:32:33 +0100

    that'd do it

--- memes.py
+++ memes.py
@@ -2,7 +2,7 @@
 from sopel.module import rule
 from sopel.formatting import bold
 
-@rule('^\[.+\]$')
+@rule('^\[(.+)\]$')
 def intensify(bot, trigger):
     bot.say(bold('[{0} INTENSIFIES]'.format(trigger.group(1).upper())))
 
