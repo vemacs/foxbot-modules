94ed80b39ee44d6243267191fe123cca0b6b7d37
Lee Watson <rev@revthefox.co.uk>
2015-09-27 09:55:25 +0100

    Clean up flow a little

--- lastfm.py
+++ lastfm.py
@@ -30,20 +30,20 @@ def now_playing(bot, trigger):
         if trigger.group(2).split(' ')[0] in add_strings:
             if len(trigger.group(2).split(' ')[1].strip()) == 0:
                 bot.reply('please provide a username. (.np -s <url>)')
-                return
-            username = trigger.group(2).split(' ')[1].strip()
-            bot.db.set_nick_value(trigger.nick, keys['lastfm'], username)
-            bot.reply('username set.')
-            return
+            else:
+                username = trigger.group(2).split(' ')[1].strip()
+                bot.db.set_nick_value(trigger.nick, keys['lastfm'], username)
+                bot.reply('username set.')
+        return
     username = bot.db.get_nick_value(trigger.nick, keys['lastfm'])
     if not username:
         bot.reply('you have no last.fm username set. Please set one with .np -s <username>')
-        return
-    network = pylast.LastFMNetwork(api_key = bot.config.lastfm.api_key)
-    user = network.get_user(username)
-    current_track = user.get_now_playing()
-    if not current_track:
-        bot.say('%s is not listening to anything right now.' % trigger.nick)
-        return
-    bot.say('%s is now playing: %s - %s | %s' % (trigger.nick, current_track.get_artist().get_name(), current_track.get_title(), color(current_track.get_url(), colors.BLUE)))
+    else:
+        network = pylast.LastFMNetwork(api_key = bot.config.lastfm.api_key)
+        user = network.get_user(username)
+        current_track = user.get_now_playing()
+        if not current_track:
+            bot.say('%s is not listening to anything right now.' % trigger.nick)
+        else:
+            bot.say('%s is now playing: %s - %s | %s' % (trigger.nick, current_track.get_artist().get_name(), current_track.get_title(), color(current_track.get_url(), colors.BLUE)))
 
