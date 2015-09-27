5bdf1426e4534b8a6b6cdbe8b7665cbc45adc17a
Lee Watson <rev@revthefox.co.uk>
2015-09-27 12:39:55 +0100

    honkhonk

--- memes.py
+++ memes.py
@@ -1,8 +1,13 @@
 # coding=utf8
-from sopel.module import rule
+from sopel.module import rule, unblockable, event
 from sopel.formatting import bold
 
 @rule('^\[(.+)\]$')
 def intensify(bot, trigger):
     bot.say(bold('[{0} INTENSIFIES]'.format(trigger.group(1).upper())))
 
+@rule('.*')
+@event("JOIN")
+@unblockable
+def honk_join(bot, trigger):
+    bot.action('honks {0}'.format(trigger.nick))
