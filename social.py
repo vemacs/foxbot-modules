2be34332c2ee815d01181de01bbeb5d2ba51abb3
Lee Watson <rev@revthefox.co.uk>
2015-09-27 01:39:05 +0100

    First test of .dtop

--- social.py
+++ social.py
@@ -1,9 +1,32 @@
 # coding=utf8
 from sopel.module import commands
 
+def filename(self):
+    name = self.nick + '-' + self.config.core.host + '.social.db'
+    return os.path.join(self.config.core.homedir, name)
+
 @commands('lasturl')
 def lasturl(bot, trigger):
     sender = trigger.sender
     if bot.memory.contains('last_seen_url') and bot.memory['last_seen_url'][sender]:
         url = bot.memory['last_seen_url'][sender]
         bot.say('[LAST URL] %s' % url)
+
+
+@commands('dtop', 'desktop')
+def dtop(bot, trigger):
+    if trigger.group(2).strip() == '':
+        desktop_url = bot.db.get_nick_value(trigger.nick, 'desktop_url')
+        if not desktop_url:
+            bot.say('You have no saved desktop!')
+            return
+        bot.say('%s [%s]' % (desktop_url, nick))
+        return
+    if trigger.group(2).split(' ')[0] == '-s':
+        if trigger.group(2).split(' ')[1].strip() == '':
+            bot.reply('Please provide a URL!')
+            return
+        url = trigger.group(2).split(' ')[1].strip()
+        bot.db.set_nick_value(trigger.nick, 'desktop_url', url)
+        bot.reply('Desktop set!')
+        return
