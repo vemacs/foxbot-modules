3f5627b1ba2b41dc814842e5ac928089350f298f
Lee Watson <rev@revthefox.co.uk>
2015-09-30 05:21:37 +0100

    Add error feedback

--- lastfm.py
+++ lastfm.py
@@ -85,6 +85,10 @@ def compare(bot, trigger):
         elif not user2:
             bot.reply('{0} has no last.fm username set. Ask them to set one with .np -s <username>'.format(user2))
             return
-        result = network.get_user(user1).compare_with_user(user2)
+        try:
+            result = network.get_user(user1).compare_with_user(user2)
+        except WSError:
+            bot.reply('last.fm API is still broken.')
+            return
         bot.reply('Result: {0}').format(result)
 
